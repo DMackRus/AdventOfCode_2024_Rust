82d46ca9e849a44d686be5f1383eeaaee0bfdd37
dmackrus <el16dmcr@leeds.ac.uk>
2024-12-10 17:42:54 +0000

    Completed part 2 of day 8

--- Day_8/src/main.rs
+++ Day_8/src/main.rs
@@ -24,16 +24,115 @@ fn main() -> io::Result<()> {
 
     // --------------------------------- PART 1 -----------------------------------------------
     // Find all the antinodes
-    let answer = find_all_antinodes(lines);
+    let mut answer = find_all_antinodes(&lines);
 
     println!("{}", answer);
 
+    // Part 2
+    answer = find_all_antinodes_part_2(&lines);
 
+    println!("{}", answer);
 
     Ok(())
 }
 
-fn find_all_antinodes(input: Vec<String>) -> i64 {
+fn find_all_antinodes_part_2(input: &Vec<String>) -> i64 {
+    let mut num_antinodes: i64 = 0;
+
+    // Store width and height of input
+    let width = input[0].chars().count();
+    let height = input.len();
+
+    // Create 2D array of booleans, all set to false
+    let mut antinodes: Vec<Vec<i64>> = vec![vec![0; width]; height];
+
+    for (i1, line) in input.iter().enumerate() {
+        for (j1, char) in line.chars().enumerate() {
+
+            for (i2, line_2) in input.iter().enumerate(){
+                for (j2, char_2) in line_2.chars().enumerate() {
+
+                    if char_2 == char && char != '.' && !((i1 == i2) && (j1 == j2)) {
+                        let all_antinodes = compute_all_antinodes_indices((j1 as i64, i1 as i64),
+                                                                          (j2 as i64, i2 as i64),
+                                                                                    width as i64, height as i64);
+
+                        for antinode in all_antinodes.iter(){
+                            antinodes[antinode.1 as usize][antinode.0 as usize] = 1;
+                        }
+
+                        println!("Pair is {:?} and {:?}", (j1, i1), (j2, i2));
+                        pretty_print(&antinodes);
+                    }
+                }
+            }
+        }
+    }
+
+    // Print the antinode 2D grid
+    pretty_print(&antinodes);
+
+    // Sum up the antinodes
+    num_antinodes = antinodes.iter()
+        .flat_map(|row| row.iter()) // Flatten the rows into a single iterator
+        .sum();
+
+
+    num_antinodes
+}
+
+fn compute_all_antinodes_indices(position_1: (i64, i64),
+                                 position_2: (i64, i64),
+                                 width: i64,
+                                 height: i64) -> Vec<(i64, i64)> {
+
+    let mut antinode_coords: Vec<(i64, i64)> = Vec::new();
+
+
+
+    let x_0 = position_1.0;
+    let y_0 = position_1.1;
+
+    let x_diff = position_2.0 - position_1.0;
+    let y_diff = position_2.1 - position_1.1;
+
+    antinode_coords.push((x_0, y_0));
+
+    // Iterate along the line
+    let mut in_bound = true;
+    let (mut new_x, mut new_y) : (i64, i64) = (x_0, y_0);
+    while(in_bound){
+        new_x += x_diff;
+        new_y += y_diff;
+
+        if check_in_bounds((new_x, new_y), width, height) {
+            antinode_coords.push((new_x, new_y));
+        }
+        else{
+            in_bound = false;
+        }
+    }
+
+    // Iterate backwards along the line
+    in_bound = true;
+    new_x = x_0;
+    new_y = y_0;
+    while(in_bound){
+        new_x -= x_diff;
+        new_y -= y_diff;
+
+        if check_in_bounds((new_x, new_y), width, height) {
+            antinode_coords.push((new_x, new_y));
+        }
+        else{
+            in_bound = false;
+        }
+    }
+
+    antinode_coords
+}
+
+fn find_all_antinodes(input: &Vec<String>) -> i64 {
     let mut num_antinodes: i64 = 0;
 
     // Store width and height of input
@@ -60,11 +159,11 @@ fn find_all_antinodes(input: Vec<String>) -> i64 {
                         // Need to check if antinode coordinate is in range
 
                         if(check_in_bounds(antinode_pair[0], width as i64, height as i64)){
-                            antinodes[antinode_pair[0].0 as usize][antinode_pair[0].1 as usize] = 1;
+                            antinodes[antinode_pair[0].1 as usize][antinode_pair[0].0 as usize] = 1;
                         }
 
                         if(check_in_bounds(antinode_pair[1], width as i64, height as i64)){
-                            antinodes[antinode_pair[1].0 as usize][antinode_pair[1].1 as usize] = 1;
+                            antinodes[antinode_pair[1].1 as usize][antinode_pair[1].0 as usize] = 1;
                         }
                     }
                 }
@@ -118,11 +217,6 @@ fn compute_antinodes_indices(position_1: (i64, i64), position_2: (i64, i64)) -> 
     let y_1 = position_1.1;
     let y_2 = position_2.1;
 
-    // m = y2 - y1 / x2 - x1
-    // let m = position_1.1 - position_1.0;
-
-    // two anti nodes - are twice as far from one node than the other
-
     let x_diff = x_2 - x_1;
     let y_diff = y_2 - y_1;
 
@@ -134,23 +228,6 @@ fn compute_antinodes_indices(position_1: (i64, i64), position_2: (i64, i64)) -> 
 
     antinodes_pair.push((x_3, y_3));
     antinodes_pair.push((x_4, y_4));
-
-    // let temp: f64 = (x_diff.pow(2) + y_diff.pow(2)) as f64;
-    // let d1: f64 = 1.0 / temp.sqrt();
-    //
-    // let cos_theta = (x_diff) as f64 / (temp.sqrt());
-    // let sin_theta = (y_diff) as f64 / (temp.sqrt());
-    //
-    // let mut x_new = x_2 + (d1 * cos_theta) as i64;
-    // let mut y_new = y_2 + (d1 * sin_theta) as i64;
-    //
-    // antinodes_pair.push((x_new, y_new));
-    //
-    // // Now compute the other antinode coordinate;
-    // x_new = x_1 - (d1 * cos_theta) as i64;
-    // y_new = y_1 - (d1 * sin_theta) as i64;
-
-    // antinodes_pair.push((x_new, y_new));
 
     antinodes_pair
 }
